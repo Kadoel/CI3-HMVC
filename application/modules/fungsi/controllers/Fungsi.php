40b025d4b74574c4440d31e3ebc4a08a3209a6a6
I Kadek Adi Suandana <mr.kadoel@gmail.com>
2015-04-18 14:21:49 +0800

    CI3HMVC v.1

---
+++ application/modules/fungsi/controllers/Fungsi.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class fungsi extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$data['welcome'] = "My Function";
+		$this->load->view('mywebsite/header',$data);
+		$this->load->view('fungsi');
+		$this->load->view('mywebsite/footer');
+	}
+}
